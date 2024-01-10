fc8ce9e8533ffaeddb947718278c50f6800ef1be
Ankitsharma991 <sharmaa7861@gmail.com>
2024-01-11 00:14:04 +0530

    updating changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-const username = "Ankitsharma991";
-const password = "A@sharmaa11";
-const databaseName = "codepath";
-
-// const uri = `mongodb+srv://${encodeURIComponent(username)}:${encodeURIComponent(password)}@cluster0.8o3ect1.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
-
-const uri = process.env.MONGODB_URI;
-
-
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
