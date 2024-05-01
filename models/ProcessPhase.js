13c30a852e3e13f9dcf71bba3928de2aa624a709
Ankitsharma991 <sharmaa7861@gmail.com>
2024-05-02 02:24:01 +0530

    initial setup

--- models/ProcessPhase.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const processPhaseSchema = new mongoose.Schema({
-  phaseName: String,
-  description: String,
-  instructions: String,
-});
-
-module.exports = mongoose.model('ProcessPhase', processPhaseSchema);
